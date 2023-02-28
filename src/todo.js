a2dd4d31b17df09c779b2b57eb1844c2bac8be72
sarahangle <sna44@cornell.edu>
2023-02-27 16:45:44 -0800

    Finish initial todo object specification

--- src/todo.js
+++ src/todo.js
@@ -1,7 +1,7 @@
 const todoFactory = (name, description, dueDate) => {
   const priority = false;
   const projectTag = null;
-  const done = false;
+  const isDone = false;
   const getName = () => name;
   const setName = (newName) => {
     name = newName;
@@ -14,13 +14,28 @@ const todoFactory = (name, description, dueDate) => {
   const setDueDate = (newDueDate) => {
     dueDate = newDueDate;
   };
+  const isDueToday = () => {
+    const todaysDate = new Date();
+    return (dueDate.setHours(0, 0, 0, 0) === todaysDate.setHours(0, 0, 0, 0));
+  };
+  const isOverdue = () => {
+    const todaysDate = new Date();
+    return (dueDate.setHours(0, 0, 0, 0) <= todaysDate.setHours(0, 0, 0, 0));
+  };
   const getPriority = () => priority;
   const togglePriority = () => {
     dueDate = !dueDate;
   };
-  const dueToday = () => {
-    const todaysDate = new Date();
-    return (dueDate.setHours(0, 0, 0, 0) === todaysDate.setHours(0, 0, 0, 0));
+  const getProjectTag = () => projectTag;
+  const setProjectTag = (newProjectTag) => {
+    projectTag = newProjectTag;
+  };
+  const getIsDone = () => isDone;
+  const setDone = () => {
+    isDone = true;
+  };
+  const setNotDone = () => {
+    isDone = false;
   };
 
   return {
@@ -30,9 +45,15 @@ const todoFactory = (name, description, dueDate) => {
     setDescription,
     getDueDate,
     setDueDate,
+    isDueToday,
+    isOverdue,
     getPriority,
     togglePriority,
-    dueToday,
+    getProjectTag,
+    setProjectTag,
+    getIsDone,
+    setDone,
+    setNotDone,
   };
 };
 
